7e55060d72854ff1d004a01a1fd6c643c1a94fc0
nareshgorinta <153359990+nareshgorinta@users.noreply.github.com>
2025-09-24 07:23:54 +0530

    Time: 3 ms (74.84%), Space: 16.5 MB (6.66%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,17 +1,59 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        int l = nums.size();
-        int count = 0;
-        for(int i=0;i<l;i++){
-            int Min = nums[i];
-            int Max = nums[i];
-            for(int j=i;j<l;j++){
-                Min = min(Min,nums[j]);
-                Max = max(Max,nums[j]);
-                count += Max - Min;
-            }
+        int n = nums.size();
+        long long result = 0;
+
+        vector<long long> maxSum(n, 0);
+        vector<long long> minSum(n, 0); 
+
+        vector<int> prevGreater(n), nextGreater(n);
+
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] < nums[i]) st.pop();
+            prevGreater[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
-        return count;
+
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) st.pop();
+            nextGreater[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            long long count = (long long)(i - prevGreater[i]) * (nextGreater[i] - i);
+            maxSum[i] = nums[i] * count;
+            result += maxSum[i];
+        }
+
+        vector<int> prevSmaller(n), nextSmaller(n);
+
+        while (!st.empty()) st.pop();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] > nums[i]) st.pop();
+            prevSmaller[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
+            nextSmaller[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        for (int i = 0; i < n; i++) {
+            long long count = (long long)(i - prevSmaller[i]) * (nextSmaller[i] - i);
+            minSum[i] = nums[i] * count;
+            result -= minSum[i];
+        }
+
+        return result;
     }
 };
