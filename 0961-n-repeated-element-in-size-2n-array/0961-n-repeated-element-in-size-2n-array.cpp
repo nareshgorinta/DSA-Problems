50035f6155a3c066eabdafb5876d461a2c11b24c
nareshgorinta <153359990+nareshgorinta@users.noreply.github.com>
2026-01-02 19:12:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        int l=nums.size();
-
-        unordered_map<int,int> freq;
-
-        for(int n:nums){
-            freq[n]++;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1])
+                return nums[i];
+            if (i + 2 < n && nums[i] == nums[i + 2])
+                return nums[i];
         }
-
-        for(auto val:freq){
-            if(val.second == l/2){
-                return val.first;
-            }
-        }
-        return 0;
+        return -1;
     }
 };
