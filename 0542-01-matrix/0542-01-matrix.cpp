6b132586393bc75f5818b3d826a86373437bde42
nareshgorinta <153359990+nareshgorinta@users.noreply.github.com>
2026-02-13 07:31:27 +0530

    Time: 4 ms (96.99%), Space: 31.6 MB (99.59%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,51 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        queue<pair<pair<int,int>,int>> q;
-
         int r = mat.size();
         int c = mat[0].size();
+        int INF = 1e9;
 
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-                if(mat[i][j] == 0){
-                    q.push({{i,j},0});
+                if(mat[i][j] != 0){
+                    int top = ( i > 0) ? mat[i-1][j] : INF;
+                    int left = (j > 0) ? mat[i][j-1] : INF;
+
+                    mat[i][j] = 1 + min(top,left);
                 }
             }
         }
 
-        while(!q.empty()){
-            int k = q.size();
-            for(int d=0;d<k;d++){
-                auto val = q.front();
-                int i = val.first.first;
-                int j = val.first.second;
-                int a = val.second;
-                q.pop();
-                vector<int>dx = {-1,0,1,0};
-                vector<int>dy = {0,-1,0,1};
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=0;j--){
+                if(mat[i][j] != 0){
+                    int bottom = (i < r-1) ? mat[i+1][j] : INF;
+                    int right = (j< c-1) ? mat[i][j+1] : INF;
 
-
-                for(int p=0;p<4;p++){
-                    int X = i + dx[p];
-                    int Y = j + dy[p];
-
-                    if( X>=0 && Y>=0 && X<r && Y<c){
-                        if(mat[X][Y] == 1){
-                            q.push({{X,Y},a+1});
-                            mat[X][Y] = - (a+1);
-                        }
-                    }
+                    mat[i][j] = min(mat[i][j],1+min(bottom,right));
                 }
-            }
-
-        }
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                mat[i][j]= abs(mat[i][j]);
             }
         }
         return mat;
-        
+
     }
 };
