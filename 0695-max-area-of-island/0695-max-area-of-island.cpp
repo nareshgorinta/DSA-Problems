2e180de317395a1ea14e47608a8315b15e3ad9c1
nareshgorinta <153359990+nareshgorinta@users.noreply.github.com>
2026-03-02 22:29:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,34 +1,64 @@
 class Solution {
 public:
-    int dfs(int i,int j,vector<vector<int>>& grid){
-        int n = grid.size();
-        int m = grid[0].size();
+    int ans  = 0;
+    int find(int a,vector<int>&parent){
+        if(a == parent[a]) return a;
+        return parent[a] = find(parent[a],parent);
+    }
 
-        grid[i][j] = 0;
+    void Union(int a,int b,vector<int>&size,vector<int>&parent){
+        int pa = find(a,parent);
+        int pb = find(b,parent);
+        if(pa == pb)return ;
 
-        vector<int> dx ={ -1,0,1,0};
-        vector<int>dy = { 0,1,0,-1};
-        int cnt  = 1;
-        for(int k=0;k<4;k++){
-            int X = i + dx[k];
-            int Y = j + dy[k];
-
-
-            if( X>= 0 && Y>=0 && X<n && Y< m  && grid[X][Y] == 1){
-                cnt+=dfs(X,Y,grid);
-            }
+        if(size[pa] < size[pb]){
+            parent[pa] = pb;
+            size[pb]+=size[pa];
+            ans = max(ans,size[pb]);
+        }else{
+            parent[pb] = pa;
+            size[pa]+=size[pb];
+            ans = max(ans,size[pa]);
         }
-        return cnt;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int ans  = 0;
-
+        vector<int>parent(m*n);
+        vector<int>size(m*n,1);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]){
-                    ans = max(ans,dfs(i,j,grid));
+                int u = m*i + j;
+                parent[u] = u;
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                // 1. Use 'm' (columns) to calculate 1D index
+                int u = m * i + j; 
+                
+                if(grid[i][j] == 1){
+                    // If the cell is land, the minimum maxArea is at least 1
+                    ans = max(ans, 1); 
+
+                    vector<int> dx = {-1, 0, 1, 0};
+                    vector<int> dy = {0, 1, 0, -1};
+
+                    for(int k=0; k<4; k++){
+                        int X = i + dx[k];
+                        int Y = j + dy[k];
+
+                        // 2. Fixed boundary check (>= instead of >)
+                        if(X < 0 || Y < 0 || X >= n || Y >= m){
+                            continue;
+                        }
+                        
+                        // 3. Check if the NEIGHBOR is also land before Union
+                        if(grid[X][Y] == 1){
+                            int v = m * X + Y; // Use 'm' here too
+                            Union(u, v, size, parent);
+                        }
+                    }
                 }
             }
         }
