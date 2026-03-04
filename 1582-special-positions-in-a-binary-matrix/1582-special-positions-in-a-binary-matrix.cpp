a435b4cd5576767fd5f117b5a207c5917c4d851b
nareshgorinta <153359990+nareshgorinta@users.noreply.github.com>
2026-03-04 10:12:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int n  = mat.size();
-        int m  = mat[0].size();
-
-        vector<int>vert(m,0);
-        vector<int>horz(n,0);
+        int n = mat.size();
+        int m = mat[0].size();
+        int cnt = 0;
 
         for(int i=0;i<n;i++){
+            int idx = -1;
+
             for(int j=0;j<m;j++){
                 if(mat[i][j] == 1){
-                    vert[j]+=1;
-                    horz[i]+=1;
+                    if(idx == -1) idx = j;
+                    else{
+                        idx = -1;
+                        break;
+                    }
                 }
             }
-        }
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]  && vert[j] == 1 && horz[i] == 1){
-                    cnt++;
+
+            if(idx != -1){
+                bool ok = true;
+
+                for(int k=0;k<n;k++){
+                    if(k != i && mat[k][idx] == 1){
+                        ok = false;
+                        break;
+                    }
                 }
+
+                if(ok) cnt++;
             }
         }
+
         return cnt;
     }
 };
